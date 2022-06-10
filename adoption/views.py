0d47be571daaed08f2a51c02cce9a8de25bccd4b
Polyana Balotin <polybalotin@hotmail.com>
2022-06-10 17:15:48 -0300

    create app adoption and configure

---
+++ adoption/views.py
@@ -0,0 +1,14 @@
+from xmlrpc.client import ResponseError
+from rest_framework.views import APIView
+from .serializers import AdoptionSerialiazer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+
+
+class AdoptionList(APIView):
+    def post(self, request, format=None):
+        serializer = AdoptionSerialiazer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
