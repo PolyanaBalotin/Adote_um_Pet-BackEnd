0397575423f9d60840ca3d49f38d02f52d096eaf
Polyana Balotin <polybalotin@hotmail.com>
2022-06-10 16:25:04 -0300

    create APIView of pet

--- pet/views.py
+++ pet/views.py
@@ -1,3 +1,11 @@
-import re
-from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Pet
+from .serializers import PetSerializer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
+class PetList(APIView):
+    def get(self, request, format=None):
+        pets = Pet.objects.all()
+        serializer = PetSerializer(pets, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
