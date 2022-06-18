f71e6df31e78bcfbf35a36bd5a09a481a492b64b
Polyana Balotin <polybalotin@hotmail.com>
2022-06-18 18:23:11 -0300

    create adoption listing action

--- adoption/views.py
+++ adoption/views.py
@@ -1,11 +1,17 @@
 from rest_framework.views import APIView
 from .serializers import AdoptionSerialiazer
 from rest_framework.response import Response
-from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from .email_service import send_confirmation_email
+from .models import Adoption
 
 
 class AdoptionList(APIView):
+    def get(self, request, format=None):
+        adoptions = Adoption.objects.all()
+        serializer = AdoptionSerialiazer(adoptions, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
     def post(self, request, format=None):
         serializer = AdoptionSerialiazer(data=request.data)
         if serializer.is_valid():
@@ -14,8 +20,8 @@ class AdoptionList(APIView):
             return Response(serializer.data, status=HTTP_201_CREATED)
         return Response(
             {
-                "errors": serializer.errors,
                 "message": "Houveram erros de validação",
+                "errors": serializer.errors,
             },
             status=HTTP_400_BAD_REQUEST,
         )
